a491139f9f5529bc356fcb2789147bcf65ea4ec8
ardafauzan <ardarizqyfauzan0301@gmail.com>
2024-04-15 16:26:27 +0700

    Hosting database

--- src/config/db_todo.js
+++ src/config/db_todo.js
@@ -2,9 +2,10 @@ const mysql = require('mysql2');
 
 // Create the connection to database
 const dbPool = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'db_todo',
+  host: 'sql204.infinityfree.com',
+  user: 'if0_36366101',
+  database: 'if0_36366101_db_todo',
+  password: '4aBWMktZ6K9r0VR'
 });
 
 module.exports = dbPool.promise()

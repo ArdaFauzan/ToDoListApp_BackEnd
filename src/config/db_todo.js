b7264efac25c3faf4eed56fa5e6915fc940b2ff9
ardafauzan <ardarizqyfauzan0301@gmail.com>
2024-04-15 22:08:17 +0700

    Try again

--- src/config/db_todo.js
+++ src/config/db_todo.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
 // Create the connection to database
-const dbPool = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'express_mysql',
+const dbPool = mysql.createPool({
+  host: 'yhh.h.filess.io',
+  user: 'todolistapp_castleupon',
+  database: 'todolistapp_castleupon',
+  password: "cc94e9945e5a87d6c571447657fa94c786137dbb",
+  port: '3307'
 });
 
-module.exports = dbPool.promise()
+module.exports = dbPool.promise();

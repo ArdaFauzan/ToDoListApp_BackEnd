4f523e91560dc9d2ce95c77f37af3b271450bac2
ardafauzan <ardarizqyfauzan0301@gmail.com>
2024-04-16 09:37:33 +0700

    Fixing database

--- src/config/db_todo.js
+++ src/config/db_todo.js
@@ -1,12 +1,8 @@
+require('dotenv').config()
+
 const mysql = require('mysql2');
 
 // Create the connection to database
-const dbPool = mysql.createPool({
-  host: 'yhh.h.filess.io',
-  user: 'todolistapp_castleupon',
-  database: 'todolistapp_castleupon',
-  password: "cc94e9945e5a87d6c571447657fa94c786137dbb",
-  port: '3307'
-});
+const dbPool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = dbPool.promise();

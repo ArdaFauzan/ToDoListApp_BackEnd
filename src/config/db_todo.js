fc5d07a5d08b3cc7df99cf8090b1dfe218cc84f0
ardafauzan <ardarizqyfauzan0301@gmail.com>
2024-05-05 16:12:45 +0700

    Fixing database

--- src/config/db_todo.js
+++ src/config/db_todo.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2');
 
 // Create the connection to database
 const dbPool = mysql.createPool({
-  host: DATABASE_HOST,
-  user: DATABASE_USER,
-  database: DATABASE,
-  port: DATABASE_PORT,
-  password: DATABASE_PASSWORD
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE,
+  port: process.env.DATABASE_PORT,
+  password: process.env.DATABASE_PASSWORD
 });
 
 dbPool.getConnection((err) => {

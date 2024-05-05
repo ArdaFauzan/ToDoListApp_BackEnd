8acaba37dc26331163eaa09420deb95e67c7cf7b
ardafauzan <ardarizqyfauzan0301@gmail.com>
2024-05-05 16:02:34 +0700

    Create database in aws

--- src/config/db_todo.js
+++ src/config/db_todo.js
@@ -3,6 +3,18 @@ require('dotenv').config()
 const mysql = require('mysql2');
 
 // Create the connection to database
-const dbPool = mysql.createPool(process.env.DATABASE_URL);
+const dbPool = mysql.createPool({
+  host: DATABASE_HOST,
+  user: DATABASE_USER,
+  database: DATABASE,
+  port: DATABASE_PORT,
+  password: DATABASE_PASSWORD
+});
 
+dbPool.getConnection((err) => {
+  if(err){
+    throw err
+  }
+  console.log('Connected to database')
+})
 module.exports = dbPool.promise();
